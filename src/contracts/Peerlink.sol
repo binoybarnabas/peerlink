843bb50f3099a9840c6d5982b31911834ada4190
binoy barnabas <binoybarnabas@gmail.com>
2023-03-18 23:53:09 +0530

    added image tipping feature using ether

--- src/contracts/Peerlink.sol
+++ src/contracts/Peerlink.sol
@@ -1,21 +1,63 @@
 pragma solidity >=0.4.21 <0.6.0;
 
 contract Peerlink {
-    // Code goes here...
+    
     string public name = "Peerlink";
-
+    //store images
+    uint public imageCount = 0;
     mapping(uint => Image) public images;
 
-    struct images{
-      uint id;
+    struct Image{
+      uint id; 
       string hash;
       string description;
       uint tipAmount;
       address payable author;
     }
-    //store images
 
+    event imageCreated
+    ( 
+      uint id,
+      string hash,
+      string description,
+      uint tipAmount,
+      address payable author
+      );
+
+    event imageTipped
+    ( 
+      uint id,
+      string hash,
+      string description,
+      uint tipAmount,
+      address payable author
+      );
+    
+    
     //create images
+    function uploadImage(string memory _imghash, string memory _description) public {
+      require(bytes(_imghash).length > 0);
+      require(bytes(_description).length > 0);
+      require(msg.sender != address(0x0));
 
+
+      //increment image
+      imageCount++;
+      //adding an image to contract
+      images[imageCount] = Image(imageCount,_imghash,_description,0,msg.sender);
+      //trigger an image
+      emit imageCreated(imageCount,_imghash,_description,0,msg.sender);
+    
+    }
     //tip images
+
+    function tipImageOwner(uint _id) public payable{
+
+      Image memory _image = images[_id];
+      address payable _author = _image.author;
+      address(_author).transfer(msg.value);
+      _image.tipAmount = _image.tipAmount + msg.value;
+      images[_id] = _image;
+      emit imageTipped(_id,_image.hash,_image.description,_image.tipAmount,_author);
+    }
 }
--- test/test.js
+++ test/test.js
@@ -23,4 +23,38 @@ contract("Peerlink", ([deployer, author, tipper]) => {
       assert.equal(name, "Peerlink");
     });
   });
+
+  describe("images", async () => {
+    let result, imageCount
+    const hash = 'asc123 '
+    before(
+      async ()=>{
+        result = await peerlink.uploadImage(hash,'Image description', {from:author})
+        imageCount = await peerlink.imageCount()
+      }
+    )
+    it("create images", async () => {
+      //success
+      assert.equal(imageCount,1)
+      const event = result.logs[0].args
+      assert.equal(event.id.toNumber(),imageCount.toNumber(),'id is correct')
+      assert.equal(event.hash,hash,'hash is correct')
+      assert.equal(event.description,'Image description','description are same')
+      assert.equal(event.tipAmount,'0','tip amount is correct')
+      assert.equal(event.author,author,'author is correct')
+
+      await peerlink.uploadImage('','Image description',{from:author}).should.be.rejected;
+
+      await peerlink.uploadImage('Image hash','',{from:author}).should.be.rejected;
+    });
+
+    it('lists images',async ()=>{
+      const image = await peerlink.images(imaegCount)
+      assert.equal(image.id.toNumber(),imageCount.toNumber(),'id is correct')
+      assert.equal(image.hash,hash,'hash is correct')
+      assert.equal(image.description,'Image description','description are same')
+      assert.equal(image.tipAmount,'0','tip amount is correct')
+      assert.equal(image.author,author,'author is correct')
+    });
+  });
 });
